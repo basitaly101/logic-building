bf1fd43ac698fe017d59c1cddd0dd8f20fa27c2e
basitaly101 <basitaly261@gmail.com>
2025-12-14 05:06:53 +0500

    first commit

---
+++ day-1/script.js
@@ -0,0 +1,11 @@
+// const arr = ['b','a','s','i','t'];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   let element = arr[i]
+// console.log(element);
+// }
+// const arr = [0,9,8,7,6,5,4,3,2,1];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   let element = arr[i]
+// console.log(element);
+// }
+
